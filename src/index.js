cf2a0d9e5af0b24bb277be7e7a96c814949141ad
George499 <53001250+George499@users.noreply.github.com>
2020-01-27 16:09:13 +0300

    mui

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
+import { theme } from "loft-taxi-mui-theme";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>,
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

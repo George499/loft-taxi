069bd1dfcfbf6c6778c328ad7e3fe3d8b3a6257b
George499 <53001250+George499@users.noreply.github.com>
2020-02-25 18:00:37 +0300

    map on component

--- src/Pages/Map/Map.js
+++ src/Pages/Map/Map.js
@@ -1,199 +1,178 @@
-import React, {
-  useState,
-  useEffect,
-  useRef,
-  useCallback,
-  useMemo
-} from "react";
-import "./Map.scss";
-import Select from "react-select";
-import mapboxgl from "mapbox-gl";
-import { Paper, Grid, FormControl, Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import newAddressList from "../../Components/PrivateRoute/index";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import {
   getCoordinatesRequest,
   getAddressListRequest,
   getChosenAdress,
   clearRoutes
 } from "../../Redux/Actions/Actions";
-import { connect } from "react-redux";
+import mapboxgl from "mapbox-gl";
+import "./Map.scss";
+import { Paper, Grid, FormControl, Button } from "@material-ui/core";
+import Select from "react-select";
+import newAddressList from "../../Components/PrivateRoute/index";
 
-const Map = props => {
-  const {
-    adressList,
-    getAddressListRequest,
-    getCoordinatesRequest,
-    getChosenAdress,
-    orderCoords,
-    clearRoutes
-  } = props;
+class Map extends Component {
+  constructor(props) {
+    super(props);
 
-  Map.propTypes = {
-    orderCoords: PropTypes.array,
-    adressList: PropTypes.array
-  };
+    // {
+    //   adressList,
+    //   getAddressListRequest,
+    //   getCoordinatesRequest,
+    //   getChosenAdress,
+    //   orderCoords,
+    //   clearRoutes
+    // };
 
-  const useStyles = makeStyles({
-    mapModal: {
-      maxWidth: "20%",
-      padding: "44px 60px",
-      marginTop: "48px",
-      marginBottom: "48px",
-      left: "20px",
-      position: "absolute",
-      top: "0"
-    },
-    modalInput: {
-      height: "auto",
-      display: "flex",
-      padding: "0"
-    }
-  });
-  const fetchedAdressess = newAddressList(adressList);
-  const [address1, changeAddress1] = useState("Город");
-  const [address2, changeAddress2] = useState("Город");
-  const [mapState] = useState({
-    lng: 30.27,
-    lat: 60,
-    zoom: 12,
-    accessToken:
-      "pk.eyJ1IjoiZ2VvcmdlaXNhZXYiLCJhIjoiY2s1ejZtM2ppMDZ2NTNncWpjcmgyMnR5NSJ9.fxa6wtBm8oLF6UNVsQeJMQ",
-    style: "mapbox://styles/georgeisaev/ck5z7ctu17adp1inwpu37v147"
-  });
-
-  const coords = useMemo(() => {
-    return orderCoords;
-  }, [orderCoords]);
-
-  let mapRef = useRef(null);
-  const [map, setMap] = useState(null);
-  const renderRoute = useCallback(() => {
-    if (map) {
-      map.addLayer({
-        id: "route",
-        type: "line",
-        source: {
-          type: "geojson",
-          data: {
-            type: "Feature",
-            properties: {
-              color: "#F7455D"
-            },
-            geometry: {
-              type: "LineString",
-              coordinates: coords
-            }
-          }
-        },
-        paint: {
-          "line-width": 8,
-          "line-color": ["get", "color"]
-        }
-      });
-      map.flyTo({
-        center: coords[0]
-      });
-    }
-  }, [map, coords]);
-
-  useEffect(() => {
-    getAddressListRequest();
-    const initializeMap = ({ setMap, mapRef }) => {
-      const map = new mapboxgl.Map({
-        container: mapRef.current,
-        center: [mapState.lng, mapState.lat],
-        zoom: mapState.zoom,
-        accessToken: mapState.accessToken,
-        style: mapState.style
-      });
-      map.on("load", () => {
-        setMap(map);
-        map.resize();
-      });
+    this.state = {
+      fetchedAdressess: newAddressList(props.adressList),
+      address1: "Город",
+      address2: "Город"
     };
+  }
 
-    if (!map) initializeMap({ setMap, mapRef });
-  }, [map, mapState, getAddressListRequest]);
-
-  useEffect(() => {
-    if (!coords && map && map.getLayer("route")) {
-      map.removeLayer("route");
-      map.removeSource("route");
-    }
-    if (coords && coords.length > 0) {
-      renderRoute();
-    }
-    if (coords.length > 0) clearRoutes();
-    return () => {};
-  }, [coords, map, renderRoute, clearRoutes]);
-
-  const handleSubmit = e => {
+  handleSubmit = e => {
     e.preventDefault();
     let addressRouts = {
-      address1: address1.value,
-      address2: address2.value
+      address1: this.state.address1,
+      address2: this.state.address2
     };
-    getChosenAdress(addressRouts);
-    getCoordinatesRequest(addressRouts);
+    console.log(this.state);
+
+    this.props.getChosenAdress(addressRouts);
+    this.props.getCoordinatesRequest(addressRouts);
+  };
+  handleFirstAdress = () => {
+    this.setState({ address1: this.address1.value });
+  };
+  handleSecondAdress = () => {
+    this.setState({ address2: this.address2 });
   };
 
-  const classes = useStyles();
+  map = null;
+  mapRef = React.createRef();
 
-  return (
-    <div style={{ position: "relative", zIndex: "-10" }}>
-      <div ref={mapRef} className="map-wrapper"></div>
-      <form onSubmit={handleSubmit}>
-        <Paper className={classes.mapModal} elevation={1}>
-          <Grid container>
-            <Grid item xs={12} style={{ marginBottom: "30px" }}>
-              <FormControl fullWidth>
-                <label className={classes.modalInput}>Откуда</label>
-                <Select
-                  value={address1}
-                  placeholder="Выбрать маршрут..."
-                  onChange={changeAddress1}
-                  className="basic-single"
-                  classNamePrefix="select"
-                  options={fetchedAdressess}
-                  isClearable
-                  isSearchable
-                />
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} style={{ marginBottom: "30px" }}>
-              <FormControl fullWidth>
-                <label className={classes.modalInput}>Куда</label>
-                <Select
-                  value={address2}
-                  placeholder="Выбрать маршрут..."
-                  onChange={changeAddress2}
-                  className="basic-single"
-                  classNamePrefix="select"
-                  options={fetchedAdressess}
-                  isClearable
-                  isSearchable
-                />
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Вызвать такси
-              </Button>
-            </Grid>
-          </Grid>
-        </Paper>
-      </form>
-    </div>
-  );
-};
+  renderRoute = () => {
+    const { orderCoords } = this.props;
+    this.map.addLayer({
+      id: "route",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {
+            color: "#ffc617"
+          },
+          geometry: {
+            type: "LineString",
+            coordinates: orderCoords
+          }
+        }
+      },
+      layout: {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      paint: {
+        "line-width": 6,
+        "line-color": "#ffc617"
+      }
+    });
 
+    this.map.flyTo({
+      center: orderCoords[0],
+      zoom: 12
+    });
+  };
+
+  componentDidMount() {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOXGL;
+    this.map = new mapboxgl.Map({
+      container: this.mapRef.current,
+      center: [30.27, 60],
+      zoom: 12,
+      accessToken:
+        "pk.eyJ1IjoiZ2VvcmdlaXNhZXYiLCJhIjoiY2s1ejZtM2ppMDZ2NTNncWpjcmgyMnR5NSJ9.fxa6wtBm8oLF6UNVsQeJMQ",
+      style: "mapbox://styles/georgeisaev/ck5z7ctu17adp1inwpu37v147"
+    });
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      const { addressCoordinates } = this.props;
+
+      if (this.map.getLayer("route")) {
+        this.map.removeLayer("route");
+        this.map.removeSource("route");
+      }
+
+      if (addressCoordinates && addressCoordinates.length > 0) {
+        this.renderRoute();
+      }
+    }
+  }
+  componentWillUnmount() {
+    this.map.remove();
+    this.props.removeCoordinatesMap();
+  }
+
+  render() {
+    console.log(this.state.address1.value);
+
+    return (
+      <div style={{ position: "relative", zIndex: "-10" }}>
+        <div ref={this.mapRef} className="map-wrapper"></div>
+        <form onSubmit={this.handleSubmit}>
+          <Paper className="mapModal" elevation={1}>
+            <Grid container>
+              <Grid item xs={12} style={{ marginBottom: "30px" }}>
+                <FormControl fullWidth>
+                  <label className="modalInput">Откуда</label>
+                  <Select
+                    value={this.state.address1}
+                    placeholder="Выбрать маршрут..."
+                    onChange={this.handleFirstAdress}
+                    className="basic-single"
+                    classNamePrefix="select"
+                    options={this.state.fetchedAdressess}
+                    isClearable
+                    isSearchable
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} style={{ marginBottom: "30px" }}>
+                <FormControl fullWidth>
+                  <label className="modalInput">Куда</label>
+                  <Select
+                    value={this.state.address2}
+                    placeholder="Выбрать маршрут..."
+                    onChange={this.handleSecondAdress}
+                    className="basic-single"
+                    classNamePrefix="select"
+                    options={this.state.fetchedAdressess}
+                    isClearable
+                    isSearchable
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                >
+                  Вызвать такси
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </form>
+      </div>
+    );
+  }
+}
 const mapStateToProps = state => {
   return {
     orderCoords: state.addressCoordinates,
